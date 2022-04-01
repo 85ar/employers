8c9ad7ef74da60199f730c9370573c716147cc45
Artur Valeev <85ar@rambler.ru>
2022-04-01 23:20:29 +0500

    base interface

--- src/components/app-filter/app-filter.js
+++ src/components/app-filter/app-filter.js
@@ -6,10 +6,10 @@ const AppFilter = () => {
       <button className="btn btn-light" type="button">
         Все сотрудники
       </button>
-      <button className="btn btn-outline-line" type="button">
+      <button className="btn btn-outline-light" type="button">
         На повышение
       </button>
-      <button className="btn btn-outline-line" type="button">
+      <button className="btn btn-outline-light" type="button">
         З/П больше 1000 $
       </button>
     </div>
